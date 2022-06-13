180da8578218de079c4b27d50d0f47ff1079cba3
aejester <aftereffectsjester@gmail.com>
2022-06-12 22:33:30 -0400

    fix issue with array differencing, old code would have had duplicates added

--- api/array_difference.go
+++ api/array_difference.go
@@ -4,12 +4,15 @@ func AlertArrayDifference(oldAlerts *[]Alert, newAlerts *[]Alert) *[]Alert {
 	var diff []Alert
 
 	for _, newAlert := range *newAlerts {
+		canAppend := true
 		for _, oldAlert := range *oldAlerts {
-			if newAlert.Description != oldAlert.Description {
-				diff = append(diff, newAlert)
+			if newAlert.Description == oldAlert.Description {
+				canAppend = false
 			}
 		}
-
+		if canAppend {
+			diff = append(diff, newAlert)
+		}
 	}
 
 	return &diff
