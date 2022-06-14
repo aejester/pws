9c3ee315a9117df1978fe2664e5887ca79fc75b7
aejester <aftereffectsjester@gmail.com>
2022-06-14 14:12:38 -0400

    precipitation_updates logic implemented

--- services/precipitation_updates.go
+++ services/precipitation_updates.go
@@ -1,7 +1,42 @@
 package services
 
-import "pws/api"
+import (
+	"fmt"
+	"pws/api"
+	"strconv"
+)
 
 func PrecipitationUpdates(weatherResponse *api.WeatherResponse, prevData *[]api.Minute) *[]api.Minute {
-	return nil
+
+	oldTimeUntil, willRainOld := minutesUntilRain(prevData)
+	newTimeUntil, willRain := minutesUntilRain(&weatherResponse.Minutely)
+
+	if oldTimeUntil != newTimeUntil {
+		var message string
+		if willRain {
+			message = "Rain expected to start in " + strconv.Itoa(newTimeUntil) + " minutes."
+		} else if willRainOld {
+			message = "It is no longer going to rain."
+		}
+
+		// TODO: Implement push notifications
+		fmt.Println(message)
+	}
+
+	return &weatherResponse.Minutely
+}
+
+func minutesUntilRain(minutes *[]api.Minute) (int, bool) {
+	if minutes != nil {
+		count := 0
+		didFindRain := false
+		for _, min := range *minutes {
+			if min.Precipitation > 0 {
+				count++
+				didFindRain = true
+			}
+		}
+		return count, didFindRain
+	}
+	return 0, false
 }
